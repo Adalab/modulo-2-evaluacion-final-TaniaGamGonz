35cc2aab19ca45c56088f1843920107f0d1d5d67
Tania Gamero González <tagago1994@gmail.com>
2022-01-03 18:48:50 +0100

    Added favourites js and run docs, finished without bonus

--- src/js/01_fetch.js
+++ src/js/01_fetch.js
@@ -4,9 +4,10 @@ const listFavourites = document.querySelector('.js-list-favourites');
 const searchBtn = document.querySelector('.js-search-btn');
 const resetBtn = document.querySelector('.js-reset-btn');
 const resetFavouritesBtn = document.querySelector('.js-resetFavourites-btn');
+let firstRender = true;
 
 let resultSearch;
-let favouriteAnimes = [];
+let favouriteAnimes = JSON.parse(localStorage.getItem("favourites")) || [] ;
 
 
 function getAnimes(anime){
@@ -23,6 +24,13 @@ function getAnimes(anime){
 
 function resetList(listToClear){
     listToClear.replaceChildren();
+}
+
+function chekcIfItsFavourite(animeToCheckId, elementToAddClass){
+    let isFavourite = favouriteAnimes.find(anime => anime.mal_id === animeToCheckId);
+    if(isFavourite){
+        elementToAddClass.classList.add('favourite');
+    }
 }
 
 function createAnimeCard(anime, listOnRender, buttonText){
@@ -42,15 +50,26 @@ function createAnimeCard(anime, listOnRender, buttonText){
     li.append(liTitle, liImg, button);
     li.dataset.id = animeId;
     li.classList.add('card');
+    if(listOnRender === listFavourites){
+        li.classList.add('favourite');
+    }
     listOnRender.appendChild(li);
-    li.addEventListener('click', setFavourite);
+    chekcIfItsFavourite(animeId, li);
+    li.addEventListener('click', (event)=>{
+        const animeId = event.currentTarget.dataset.id;
+        setFavourite(animeId);
+        event.currentTarget.classList.toggle('favourite');
+    });
 }
 
-function setFavourite(event){
-   const animeId = event.currentTarget.dataset.id;
+function setFavourite(animeId){
+   const animeAlreadyInFavourites = searchAnimeById(animeId, favouriteAnimes);
    const anime =  searchAnimeById(animeId, resultSearch);
    renderFavourites(anime);
+   if(!animeAlreadyInFavourites){
    favouriteAnimes.push(anime);
+   localStorage.setItem("favourites", JSON.stringify(favouriteAnimes));
+   }
 }
 
 function searchAnimeById(animeId, arrayToSearch){
@@ -59,10 +78,17 @@ function searchAnimeById(animeId, arrayToSearch){
 
 function renderFavourites(anime){
     const animeAlreadyInFavourites = searchAnimeById(anime.mal_id, favouriteAnimes);
-    if(!animeAlreadyInFavourites){
+
+    if(firstRender){
+        createAnimeCard(anime, listFavourites, 'Borrar de favoritos');
+    }else if(!animeAlreadyInFavourites){
     createAnimeCard(anime, listFavourites, 'Borrar de favoritos');
     }
 }
+
+
+//Render favourites at the start of the page 
+favouriteAnimes.forEach( anime => renderFavourites(anime));
 
   searchBtn.addEventListener('click', (event) => {
       const animeSearched = document.querySelector('#anime').value;
@@ -70,9 +96,11 @@ function renderFavourites(anime){
       resetList(listResult);
       getAnimes(animeSearched);
   })
-  resetBtn.addEventListener('click', (event)=>{
+  resetBtn.addEventListener('click', ()=>{
       resetList(listResult);
   });
-  resetFavouritesBtn.addEventListener('click', (event)=>{
+  resetFavouritesBtn.addEventListener('click', () =>{
     resetList(listFavourites);
+    favouriteAnimes = [];
+    localStorage.removeItem("favourites");
 });
